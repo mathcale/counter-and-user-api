e43d2dc95447c7fcb4929c9434944e892c167b14
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-05-01 19:42:36 -0300

    refactor(counter-api/*): using code from shared lib

--- packages/functions/counter-api/increment-fn/index.js
+++ packages/functions/counter-api/increment-fn/index.js
@@ -1,16 +1,6 @@
 const axios = require('axios').default;
+const { utils } = require('desafio-ton-stone-lib');
 const { COUNTAPI_URL, COUNTAPI_KEY, COUNTAPI_NAMESPACE } = process.env;
-
-const buildResonse = (statusCode, headers = {}, body = null) => {
-  return {
-    statusCode,
-    headers: {
-      'Content-Type': 'application/json',
-      ...headers,
-    },
-    body: body && JSON.stringify(body),
-  };
-};
 
 exports.handler = async (event) => {
   console.info('Incrementando contagem de acessos ao site do Ton...');
@@ -19,10 +9,10 @@ exports.handler = async (event) => {
     const response = await axios.get(`${COUNTAPI_URL}/hit/${COUNTAPI_NAMESPACE}/${COUNTAPI_KEY}`);
     console.info(`Contador incrementado com sucesso para ${response.data.value}!`);
 
-    return buildResonse(200, null, { newValue: response.data.value });
+    return utils.buildHttpResonse(200, null, { newValue: response.data.value });
   } catch (err) {
     console.error(err);
 
-    return buildResonse(500, null, { message: err.message });
+    return utils.buildHttpResonse(500, null, { message: err.message });
   }
 };
--- packages/functions/counter-api/show-fn/index.js
+++ packages/functions/counter-api/show-fn/index.js
@@ -1,16 +1,6 @@
 const axios = require('axios').default;
+const { utils } = require('desafio-ton-stone-lib');
 const { COUNTAPI_URL, COUNTAPI_KEY, COUNTAPI_NAMESPACE } = process.env;
-
-const buildResonse = (statusCode, headers = {}, body = null) => {
-  return {
-    statusCode,
-    headers: {
-      'Content-Type': 'application/json',
-      ...headers,
-    },
-    body: body && JSON.stringify(body),
-  };
-};
 
 exports.handler = async (event) => {
   console.info('Buscando contagem de acessos ao site do Ton...');
@@ -19,10 +9,10 @@ exports.handler = async (event) => {
     const response = await axios.get(`${COUNTAPI_URL}/get/${COUNTAPI_NAMESPACE}/${COUNTAPI_KEY}`);
     console.info(`Valor atual do contador: ${response.data.value}`);
 
-    return buildResonse(200, null, { value: response.data.value });
+    return utils.buildHttpResonse(200, null, { value: response.data.value });
   } catch (err) {
     console.error(err);
 
-    return buildResonse(500, null, { message: err.message });
+    return utils.buildHttpResonse(500, null, { message: err.message });
   }
 };
