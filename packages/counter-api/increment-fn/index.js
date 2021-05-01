050a93f63adbdfec0e47a8948ef7a3143acc5bde
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-05-01 18:18:17 -0300

    feat(counter-api/increment): implementing main logic

--- packages/counter-api/increment-fn/index.js
+++ packages/counter-api/increment-fn/index.js
@@ -1,9 +1,28 @@
-module.exports = async (event) => {
+const axios = require('axios').default;
+const { COUNTAPI_URL, COUNTAPI_KEY, COUNTAPI_NAMESPACE } = process.env;
+
+const buildResonse = (statusCode, headers = {}, body = null) => {
   return {
-    statusCode: 200,
+    statusCode,
     headers: {
       'Content-Type': 'application/json',
+      ...headers,
     },
-    body: JSON.stringify({ message: 'hello' }),
+    body: body && JSON.stringify(body),
   };
+};
+
+exports.handler = async (event) => {
+  console.info('Incrementando contagem de acessos ao site do Ton...');
+
+  try {
+    const response = await axios.get(`${COUNTAPI_URL}/hit/${COUNTAPI_NAMESPACE}/${COUNTAPI_KEY}`);
+    console.info(`Contador incrementado com sucesso para ${response.data.value}!`);
+
+    return buildResonse(200, null, { newValue: response.data.value });
+  } catch (err) {
+    console.error(err);
+
+    return buildResonse(500, null, { message: err.message });
+  }
 };
