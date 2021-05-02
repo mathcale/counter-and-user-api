5a8346973aa495d7e5992c29858c7b00bbcce4b3
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-05-01 23:53:51 -0300

    chore(scripts/run-local): updating layer with fresh code from lib, add more logs

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -12,6 +12,15 @@ if ! [ -x "$(command -v sam)" ]; then
   die "AWS SAM CLI is mandatory to proceed!"
 fi
 
+echo "[INFO] Copying 'lib' code to 'layer'..."
+
+rm -rf packages/layer/nodejs/node_modules/desafio-ton-stone-lib
+cp -R packages/lib packages/layer/nodejs/node_modules/desafio-ton-stone-lib
+
+echo "[INFO] Starting local API..."
+
 sam local start-api \
   -t infra/$PROJECT/template-$PROJECT-application.yaml \
   --warm-containers LAZY
+
+echo -e "\n[INFO] Interruption received"
